d60d21fe3c7b5568758419b49937eb571627c3bc
Marcus  = Soulo <souloinc87@gmail.com>
2018-11-06 20:12:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,11 +103,11 @@ class Student
   end
 
   def self.first_X_students_in_grade_10
-    sql = <<-SQL 
-      SELECT * 
-      FROM students 
-      WHERE grade = 10 
-      ORDER BY students.id 
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id
       LIMIT 1
     SQL
 
